cc86bb9a2956440fb180f02fb1f6a0611bbdac46
Rakandz7 <rakanlerouge@gmail.com>
2023-02-28 11:48:28 +0100

    visualisationC.c

--- visualisationC.c
+++ visualisationC.c
@@ -4,8 +4,50 @@
 #include "visualisationC.h"
 
 
-   void visualisationC(float puissance_f)
-   {
+   void visualisationC(float puissance_f){
+    if (access (".verrouData",F_OK)!= -1){
+
+       }
+    else {
+        FILE *verrouData= NULL;
+        verrouData = fopen(".verrouData", "w");
+        if (verrouData == NULL) {
+            fprintf(stderr, "le fichier n'a pas pu etre creer  \n");
+            exit(0);
+
+        }
+        FILE* data = NULL;
+        data = fopen("data.txt","r");
+        float tempExt;
+        float tempInt;
+        char ioChauff;
+        if (data != NULL){
+            fscanf(data, "‰f", &tempExt); // on lit la première ligne
+            fscanf(data, "%f", &tempInt); // on lit la deuxième ligne
+            fscanf(data, "%s", ioChauff); // on lit la trosième ligne
+            fclose(data);
+        }
+        else{
+            printf("Impossible d'ouvrir data.txt");
+        }
+        data = fopen("data.txt","w");
+        fprintf(data,"%f",tempExt);
+        fprintf(data,"%f",tempInt);
+        if(puissance_f==0){
+            fprintf(data,"false");
+            fclose(data);
+        }
+        else {
+            fprintf(data,"true");
+            fclose(data);
+        }
+        remove(".verrouData");
+    }
+
+
+
+
+   }
             	
    
    }
