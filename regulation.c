df12c3f43e33a9ef3939ee9adc7eaa3b51eb45b6
marius2468 <mariusfremond@isen-ouest.yncrea.fr>
2023-02-28 13:35:39 +0100

    simulateur.c

--- regulation.c
+++ regulation.c
@@ -1,11 +1,16 @@
-    #include <stdio.h>
-	 #include <stdlib.h>
-    #include "regulation.h"
-	 
-	float regulationTest(int regul, float consigne, float* tabT, int nT){
-		float cmd;
+#include <stdio.h>
+#include <stdlib.h>
+#include "regulation.h"
 
-		return cmd;
-	}
-    
-  
+float regulationTest(int regul, float consigne, float* tabT, int nT){
+    float cmd;
+    if (regul){
+        if (consigne > tabT[nT-1]){
+            cmd = 40.0;
+        }
+        else{
+            cmd = 0.0;
+        }
+    }
+    return cmd;
+}
